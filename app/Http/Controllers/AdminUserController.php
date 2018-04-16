0ea17a38c17ee90da0d1d49b02ede872758f98ec
Abbass <abbassmortazavi@gmail.com>
2018-04-16 12:25:43 +0430

    last commit film 219

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -143,7 +143,8 @@ class AdminUserController extends Controller
     public function destroy($id)
     {
         $user = User::findOrFail($id);
-
+        unlink(public_path(). "/" . $user->photo->file);
+        $user->delete();
         Session::flash('delete_user' , 'this user has been deleted!!!');
         return redirect(route('users.index'));
     }
